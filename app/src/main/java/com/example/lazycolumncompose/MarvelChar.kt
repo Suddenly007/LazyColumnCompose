d830d861c4138b66f093fe8ceb835a4689181bfd
Suddenly007 <mussambwana1@gmail.com>
2025-04-15 21:37:28 +0300

    update

--- app/src/main/java/com/example/lazycolumncompose/MarvelChar.kt
+++ app/src/main/java/com/example/lazycolumncompose/MarvelChar.kt
@@ -8,9 +8,23 @@ data class MarvelChar(
 
 fun getMarvelChars(): List<MarvelChar>{
     return listOf(
-        MarvelChar("Spiderman", "Peter Parker", R.drawable.dn),
+        MarvelChar("Spiderman", "Peter Parker", R.drawable.spider),
         MarvelChar("Iron Man", "Tony Stark", R.drawable.iron),
-        MarvelChar("Captain America", "Steve Rogers", R.drawable.thanos),
+        MarvelChar("Thanos", "Josh Brolin", R.drawable.thanos),
+        MarvelChar("Captain America", "Steve Rogers", R.drawable.captain),
+        MarvelChar("Ant-Man", "Scott Lang", R.drawable.antman),
+        MarvelChar("Arrow", "Oliver Queen", R.drawable.arrow),
+        MarvelChar("Spiderman", "Peter Parker", R.drawable.spider),
+        MarvelChar("Iron Man", "Tony Stark", R.drawable.iron),
+        MarvelChar("Thanos", "Josh Brolin", R.drawable.thanos),
         MarvelChar("Captain America", "Steve Rogers", R.drawable.thor),
+        MarvelChar("Ant-Man", "Scott Lang", R.drawable.antman),
+        MarvelChar("Arrow", "Oliver Queen", R.drawable.arrow),
+        MarvelChar("Spiderman", "Peter Parker", R.drawable.spider),
+        MarvelChar("Iron Man", "Tony Stark", R.drawable.iron),
+        MarvelChar("Thanos", "Josh Brolin", R.drawable.thanos),
+        MarvelChar("Captain America", "Steve Rogers", R.drawable.thor),
+        MarvelChar("Ant-Man", "Scott Lang", R.drawable.antman),
+        MarvelChar("Arrow", "Oliver Queen", R.drawable.arrow),
         )}
 
